572c1a86e8b8fbd423d61c86d89058db65a213b3
Rohit Mittal <mittalr685@gmail.com>
2022-10-09 22:00:47 -0500

    removed some code

--- index.py
+++ index.py
@@ -6,8 +6,3 @@ bp = Blueprint("index", __name__)
 @bp.route("/")
 def index():
 	return render_template("index.html")
-
-@bp.route("/rickroll")
-def rickroll():
-	#webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-	return render_template("rickroll.html")
